c93cf0e3942c64590bc9d58f887f629f51f88e36
Erdem Ersoy <erdemersoy256@gmail.com>
2024-06-30 15:32:52 +0300

    Some changes:
    - kernel32: Add initial GetVersion()
    - emulator: Add initial version checking

---
+++ dlls/kernel32.py
@@ -0,0 +1,5 @@
+class Kernel32:
+    @staticmethod
+    def GetVersion():
+        # Windows NT 4.0 SP6a version number
+        return 0x00040006
--- emulator.py
+++ emulator.py
@@ -3,6 +3,8 @@ from unicorn.x86_const import *
 
 import colorama
 
+from dlls.kernel32 import Kernel32  # Local package!!!
+
 class Emulator:
     EXIT_SUCCESS = 0
     EXIT_FAILURE = 1
@@ -19,6 +21,11 @@ class Emulator:
 
     def emulate(self, entry_point, exe_code_size):
         try:
+            if not self.get_emulator_win_version_is_nt():
+                print(colorama.Fore.YELLOW + "WARNING: " + colorama.Fore.RESET + "The emulation is NOT as" + colorama.Fore.CYAN + " Windows NT" + colorama.Fore.RESET + "!!!")
+
+            print("Emulation as: " + colorama.Fore.CYAN + f"{self.get_emulator_win_version_str()}" + colorama.Fore.RESET)
+
             self.cpu_emulator.emu_start(entry_point, entry_point + exe_code_size)
         except UcError as e:
             colorama.init()
@@ -30,3 +37,30 @@ class Emulator:
             self.cpu_emulator.close()
 
             return Emulator.EXIT_SUCCESS
+    
+    def get_emulator_win_version_is_nt(self):
+        ver = Kernel32.GetVersion()
+
+        if ver < 0x80000000:
+            return True
+        else:
+            return False
+
+    def get_emulator_win_version_str(self):
+        ver = Kernel32.GetVersion()
+        ver_major = ver >> 24 & 0xFF
+        ver_minor = ver >> 16 & 0xFF
+        ver_build = 0x0000
+        ver_str = ""
+
+        if ver < 0x80000000:
+            ver_build = ver & 0xFFFF
+            ver_str = "Windows NT"
+        elif ver_major < 4:
+            ver_build = (ver & 0xFFFF) & ~0x8000
+            ver_str = "Win32s"
+        else:
+            ver_build = ver & 0xFFFF
+            ver_str = "Windows 95"
+
+        return ver_str
